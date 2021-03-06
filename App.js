785c95f0b0c7f7913587037a897105a256cf6e3e
Quang Trung <dotrunghg1999@gmail.com>
2021-03-06 11:01:15 +0700

    set up firebase

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Provider } from "react-redux";
 import store from "./src/redux/store";
+import { Text } from "react-native";
+import * as firebase from "firebase";
+import { firebaseConfig } from "./src/constants/config";
 
-var firebaseConfig = {
-  apiKey: "AIzaSyC6IcCsOSlNDqeGIKUOvtpSjOANaDoeugg",
-  authDomain: "tour-guide-a2e70.firebaseapp.com",
-  projectId: "tour-guide-a2e70",
-  storageBucket: "tour-guide-a2e70.appspot.com",
-  messagingSenderId: "383628957928",
-  appId: "1:383628957928:web:81f477dcf095ba8fd7115d",
-  measurementId: "G-KBCSG9YQD1",
-};
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
-  return <Provider store={store}></Provider>;
+  return (
+    <Provider store={store}>
+      <Text> hello word</Text>
+    </Provider>
+  );
 }
---
+++ src/constants/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC6IcCsOSlNDqeGIKUOvtpSjOANaDoeugg",
+  authDomain: "tour-guide-a2e70.firebaseapp.com",
+  projectId: "tour-guide-a2e70",
+  storageBucket: "tour-guide-a2e70.appspot.com",
+  messagingSenderId: "383628957928",
+  appId: "1:383628957928:web:81f477dcf095ba8fd7115d",
+  measurementId: "G-KBCSG9YQD1",
+};
