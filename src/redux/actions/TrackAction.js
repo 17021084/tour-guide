bd1df7ffb234c08948d9af70eadcd4ee6743dedd
Quang Trung <dotrunghg1999@gmail.com>
2021-03-14 22:35:55 +0700

    define action for tracking

--- src/redux/actions/TrackAction.js
+++ src/redux/actions/TrackAction.js
@@ -1,1 +1,37 @@
-// export
+import {
+  CURRENT_JOURNEY_NAME_CHANGE,
+  CURRENT_JOURNEY_POINTS_LIST_CHANGE,
+  JOURNEY_RESET,
+  TRACKING_STATUS_CHANGE,
+} from "./type";
+
+export const journeyReset = () => {
+  return {
+    action: JOURNEY_RESET,
+  };
+};
+
+export const fetchJourneyList = () => {
+  return (dispatch) => {};
+};
+
+export const changeTrackingStatus = (status) => {
+  return {
+    type: TRACKING_STATUS_CHANGE,
+    payload: status,
+  };
+};
+
+export const changeCurrentJourneyName = (name) => {
+  return {
+    type: CURRENT_JOURNEY_NAME_CHANGE,
+    payload: name,
+  };
+};
+
+export const changeCurrentJournyPointList = (point) => {
+  return {
+    type: CURRENT_JOURNEY_POINTS_LIST_CHANGE,
+    payload: point,
+  };
+};
--- src/screens/AuthScreens/JourneyScreens/JourneyTrack.js
+++ src/screens/AuthScreens/JourneyScreens/JourneyTrack.js
@@ -13,44 +13,44 @@ function JourneyTrack({ navigation }) {
   const [currentCoord, setCurrentCoord] = useState([]);
   const [shouldTrack, setShouldStrack] = useState(false);
   const list = useRef([]);
-  useEffect(() => {
-    let subscriber = null;
+  // useEffect(() => {
+  //   let subscriber = null;
 
-    const startTracking = async () => {
-      try {
-        let { status } = await Location.requestPermissionsAsync();
-        if (status !== "granted") {
-          setError("Permission to access location was denied");
-        }
-        subscriber = await Location.watchPositionAsync(
-          {
-            accuracy: Location.Accuracy.BestForNavigation,
-            timeInterval: 3000, //* 1s
-            distanceInterval: 10, //* 10 meters
-          },
-          callback
-        );
-      } catch (err) {
-        setError(err);
-      }
-    };
-    if (shouldTrack) {
-      startTracking();
-    } else {
-      if (subscriber) {
-        subscriber.remove();
-        console.log("remove");
-      }
-      subscriber = null;
-    }
+  //   const startTracking = async () => {
+  //     try {
+  //       let { status } = await Location.requestPermissionsAsync();
+  //       if (status !== "granted") {
+  //         setError("Permission to access location was denied");
+  //       }
+  //       subscriber = await Location.watchPositionAsync(
+  //         {
+  //           accuracy: Location.Accuracy.BestForNavigation,
+  //           timeInterval: 3000, //* 1s
+  //           distanceInterval: 10, //* 10 meters
+  //         },
+  //         callback
+  //       );
+  //     } catch (err) {
+  //       setError(err);
+  //     }
+  //   };
+  //   if (shouldTrack) {
+  //     startTracking();
+  //   } else {
+  //     if (subscriber) {
+  //       subscriber.remove();
+  //       console.log("remove");
+  //     }
+  //     subscriber = null;
+  //   }
 
-    return () => {
-      if (subscriber) {
-        console.log("remove in return");
-        subscriber.remove();
-      }
-    };
-  }, [shouldTrack, callback]);
+  //   return () => {
+  //     if (subscriber) {
+  //       console.log("remove in return");
+  //       subscriber.remove();
+  //     }
+  //   };
+  // }, [shouldTrack, callback]);
 
   const [coord2, setCoord2] = useState([]);
 
@@ -71,24 +71,18 @@ function JourneyTrack({ navigation }) {
     Location.stopLocationUpdatesAsync("TRACK");
   };
 
-  const callback = (location) => {
-    console.log(
-      "dang track ============== at",
-      new Date().toLocaleTimeString()
-    );
-    console.log(location);
+  // const callback = (location) => {
+  //   console.log(
+  //     "dang track ============== at",
+  //     new Date().toLocaleTimeString()
+  //   );
+  //   console.log(location);
 
-    // newCoord.push(location);
-    setCoord([...coord, location]);
-  };
+  //   // newCoord.push(location);
+  //   setCoord([...coord, location]);
+  // };
 
-  // console.log("testCoord");
-  // console.log(testCoord);
-  // console.log("coord=====");
-  // console.log(coord);
-  // console.log("current Coord=====");
-  // console.log(currentCoord);
-  console.log("    - length  = ", coord2.length);
+  console.log("Length  = ", coord2.length);
 
   return (
     <View>
@@ -99,18 +93,21 @@ function JourneyTrack({ navigation }) {
       <Text>Dia chi bat dau: </Text>
       <Text>Dia chi Hien tai: </Text>
       <Text>Button xem ong hien tai : </Text>
-      <Button
-        title={"bat trackking"}
-        onPress={() => {
-          setShouldStrack(true);
-        }}
-      />
-      <Button
-        title={"tat tracking"}
-        onPress={() => {
-          setShouldStrack(false);
-        }}
-      />
+      {
+        // <Button
+        //   title={"bat trackking"}
+        //   onPress={() => {
+        //     setShouldStrack(true);
+        //   }}
+        // />
+        // <Button
+        //   title={"tat tracking"}
+        //   onPress={() => {
+        //     setShouldStrack(false);
+        //   }}
+        // />
+      }
+
       <Button title={"recored"} onPress={startRecord} />
       <Button title={"tat record"} onPress={stopRecord} />
 
