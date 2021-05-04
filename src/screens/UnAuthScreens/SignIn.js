94e4c5a5b6007d9cb6369b7014b22328c3c38ddb
Quang Trung <dotrunghg1999@gmail.com>
2021-05-04 13:25:01 +0700

    prepare for deploy

--- src/screens/UnAuthScreens/SignIn.js
+++ src/screens/UnAuthScreens/SignIn.js
@@ -14,7 +14,7 @@ import ButtonBox from "../../components/common/ButtonBox";
 import InputBox from "../../components/common/InputBox";
 import { color } from "../../config/appConfig";
 import { validateEmail } from "../../utils/validateEmail";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
 import firebase from "firebase";
 import "firebase/firestore";
 export default function SignIn({ navigation }) {
@@ -70,43 +70,45 @@ export default function SignIn({ navigation }) {
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={styles.container}
     >
-      <View>
-        <Image style={styles.image} source={logo} />
-      </View>
-      <Text style={styles.error}>{error}</Text>
-      <View style={styles.infor}>
-        <View style={styles.input}>
-          <InputBox
-            title="Email"
-            placeholder="test1@gmail"
-            value={data.email}
-            onChangeText={emailChange}
-          />
+      <ScrollView>
+        <View>
+          <Image style={styles.image} source={logo} />
         </View>
-        <View style={styles.input}>
-          <InputBox
-            title="Mật khẩu"
-            placeholder="6ký tự trở lên"
-            secureTextEntry={true}
-            value={data.password}
-            onChangeText={passwordChange}
-          />
-        </View>
+        <Text style={styles.error}>{error}</Text>
+        <View style={styles.infor}>
+          <View style={styles.input}>
+            <InputBox
+              title="Email"
+              placeholder="test1@gmail"
+              value={data.email}
+              onChangeText={emailChange}
+            />
+          </View>
+          <View style={styles.input}>
+            <InputBox
+              title="Mật khẩu"
+              placeholder="6ký tự trở lên"
+              secureTextEntry={true}
+              value={data.password}
+              onChangeText={passwordChange}
+            />
+          </View>
 
-        <Text style={styles.signUp}>
-          Bạn đã có tài khoản chưa ?
-          <TouchableOpacity
-            onPress={() => navigation.navigate(SIGN_UP_SCREEN)}
-            style={styles.signUpButton}
-          >
-            <Text style={styles.signUpText}> Đăng ký</Text>
-          </TouchableOpacity>
-        </Text>
+          <Text style={styles.signUp}>
+            Bạn đã có tài khoản chưa ?
+            <TouchableOpacity
+              onPress={() => navigation.navigate(SIGN_UP_SCREEN)}
+              style={styles.signUpButton}
+            >
+              <Text style={styles.signUpText}> Đăng ký</Text>
+            </TouchableOpacity>
+          </Text>
 
-        <View style={styles.button}>
-          <ButtonBox title="Đăng nhập " onPress={signIn} />
+          <View style={styles.button}>
+            <ButtonBox title="Đăng nhập " onPress={signIn} />
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </KeyboardAvoidingView>
   );
 }
@@ -139,7 +141,7 @@ const styles = StyleSheet.create({
   },
   signUp: {
     marginTop: 20,
-    marginBottom: 100,
+    marginBottom: 20,
   },
   signUpButton: {
     paddingTop: 3,
